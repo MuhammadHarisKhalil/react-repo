079ad0698ceaf7c273ffa7b4ca3894576a0b20a4
haris3474 <haris.khalil@manafatech.com>
2023-05-27 12:26:43 +0500

    Like section Complete

--- src/component/screens/Home.js
+++ src/component/screens/Home.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react";
-
+import React, { useState, useEffect, useContext } from "react";
+import {UserContext} from '../../App';
 const Home = () => {
   const [data, setData ] = useState([]);
+  const {state,dispatch} =useContext(UserContext)
   //console.log(localStorage.getItem("jwt"))
   useEffect(() => {
     fetch("/allpost", {
@@ -15,6 +16,60 @@ const Home = () => {
         setData(result.post);
       });
   }, []);
+
+  const likes = (id)=>{
+    fetch('/like', {
+      method: 'put',
+      headers :{
+        'Content-Type' : "application/json",
+        'Authorization' : "Bearer " + localStorage.getItem("jwt")
+      },
+      body : JSON.stringify({
+        postId : id
+        })
+      }).then(res=>res.json())
+        .then(result=>{
+          //console.log(result)
+
+          const newData = data.map(item=>{
+            if(item._id == result._id){
+                return result
+            }else{
+               return item
+            }
+          })
+          setData(newData)
+          
+        }).catch(err=>{
+          console.log(err)
+        })
+  };
+  const unlikes = (id)=>{
+    fetch('/unlike', {
+      method: 'put',
+      headers :{
+        'Content-Type' : "application/json",
+        'Authorization' : "Bearer " + localStorage.getItem("jwt")
+      },
+      body : JSON.stringify({
+        postId : id
+        })
+        }).then(res=>res.json())
+        .then(result=>{
+          //console.log(result)
+
+          const newData = data.map(item=>{
+            if(item._id == result._id){
+                return result
+            }else{
+               return item
+            }
+          })
+            setData(newData)
+        }).catch(err=>{
+          console.log(err)
+        })
+  }
   return (
     <div className="home">
       {
@@ -29,6 +84,13 @@ const Home = () => {
           <i className="material-icons" style={{ color: "red" }}>
             favorite
           </i>
+          {item.likes.includes(state._id)
+          ?
+          <i className="material-icons" onClick={()=>unlikes(item._id)} >thumb_down</i>
+          :
+          <i className="material-icons" onClick={()=>likes(item._id)}>thumb_up</i>
+          }
+          <h6>{item.likes.length} likes</h6>
           <h6>{item.title}</h6>
           <p>{item.body}</p>
           <input type="text" placeholder="add a comment" />
